1fe3c94ed2b8e0c33904a426cadedf720d957971
wled-install <96419931+wled-install@users.noreply.github.com>
2026-03-15 11:28:16 +0100

    Add files via upload

---
+++ APP_FUNCTIONS/safe_boot_functions.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef  WLED_ENABLE_SAFE_BOOT
+#include <esp_partition.h>
+#include <esp_spi_flash.h>
+#include <rom/crc.h>
+#include <esp_ota_ops.h>
+
+bool update_spiffs_crc();
+void update_ota_crc();
+
+#endif
