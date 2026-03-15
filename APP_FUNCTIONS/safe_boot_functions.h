9cd82bd05592fa617c47b2bd11daaf6e92202d26
wled-install <96419931+wled-install@users.noreply.github.com>
2026-03-15 19:48:12 +0100

    Add files via upload

--- APP_FUNCTIONS/safe_boot_functions.h
+++ APP_FUNCTIONS/safe_boot_functions.h
@@ -1,3 +1,8 @@
+/*
+ This functions are used to use WLED together with "Safe Bootbootloader"
+ https://github.com/wled-install/SafeBootloader
+*/
+
 #pragma once
 
 #ifdef  WLED_ENABLE_SAFE_BOOT
@@ -7,6 +12,5 @@
 #include <esp_ota_ops.h>
 
 bool update_spiffs_crc();
-void update_ota_crc();
 
 #endif
